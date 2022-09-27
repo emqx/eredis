05b0716b63aeae214aa981baaa5ae04bd7f9d63a
Zaiming (Stone) Shi <zmstone@gmail.com>
2022-09-27 10:53:21 +0200

    fix: hide password

---
+++ src/eredis_secret.erl
@@ -0,0 +1,24 @@
+%% Note: this module CAN'T be hot-patched to avoid invalidating the
+%% closures, so it must not be changed.
+-module(eredis_secret).
+
+%% API:
+-export([wrap/1, unwrap/1]).
+
+%%================================================================================
+%% API funcions
+%%================================================================================
+
+wrap(undefined) -> undefined;
+wrap(Func) when is_function(Func, 0) ->
+    Func;
+wrap(Term) ->
+    fun() ->
+        Term
+    end.
+
+unwrap(Term) when is_function(Term, 0) ->
+    %% Handle potentially nested funs
+    unwrap(Term());
+unwrap(Term) ->
+    Term.
