e8aee7f3a153dcdf7dec881ee88626ac64f8915a
Zaiming (Stone) Shi <zmstone@gmail.com>
2022-09-28 14:06:27 +0200

    fix: implement format_status callback to hide password

--- src/eredis_client.erl
+++ src/eredis_client.erl
@@ -30,7 +30,7 @@
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
+         terminate/2, code_change/3, format_status/2]).
 
 -export([reconnect_loop/3]).
 
@@ -226,6 +226,11 @@ terminate(_Reason, State) ->
 %% Down
 code_change(_, State, _) ->
     {ok, State}.
+
+format_status(_Opt, [_PDict, #state{} = State]) ->
+    [{data, [{"State", State#state{password = "******"}}]}];
+format_status(_Opt, [_PDict, State]) ->
+    [{data, [{"State", State}]}],
 
 close(_Transport, undefined) -> ok;
 close(Transport, Socket) ->
